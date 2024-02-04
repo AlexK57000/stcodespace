596f289ea9f0763ae381a0c9ea85ba7b6c60b4a4
AlexK57000 <158551373+AlexK57000@users.noreply.github.com>
2024-02-04 21:59:32 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -19,38 +19,6 @@ yer = mean_absolute_error(102, 92)
 
 st.write(yer)
 
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Alex",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋 Bienvenue à bord")
-
-    st.sidebar.success("Select a demo above and beyond.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
 if __name__ == "__main__":
